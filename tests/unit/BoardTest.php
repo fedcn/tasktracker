c750edb11c7aa936b06f3d518e76e672a1569175
Yury G <golovanovya@gmail.com>
2020-07-17 22:39:48 +0300

    add board tests

---
+++ tests/unit/BoardTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests;
+
+use App\Tasktracker\Entities\Board;
+use App\Tasktracker\Entities\User;
+use Codeception\Test\Unit;
+
+class BoardTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSuccessfulCreate()
+    {
+        $owner = new User('testUser', 'testuser@gmail.com');
+        $board = new Board('testBoard', $owner);
+        $this->assertEquals('testBoard', $board->getName());
+        $this->assertEquals($owner, $board->getOwner());
+        $this->assertEquals([], $board->getParticipants());
+        $this->assertEquals(null, $board->getDescription());
+    }
+    
+    public function testEmpty()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $owner = new User('testUser', 'testuser@gmail.com');
+        new Board();
+        new Board('');
+        new Board('', $owner);
+    }
+}
