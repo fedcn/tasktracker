0ca15434df8530031e4335d2041ff5ea80dd1c9a
Разиков Алексей <razikov88@yandex.ru>
2020-07-22 22:53:31 +0300

    fix

--- src/Tasktracker/Repository/BoardDoctrineRepository.php
+++ src/Tasktracker/Repository/BoardDoctrineRepository.php
@@ -2,7 +2,28 @@
 
 namespace App\Repository;
 
-interface BoardDoctrineRepository
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use App\Tasktracker\Entity\Board;
+
+class BoardRepository extends ServiceEntityRepository implements BoardRepository
 {
-    public function findAll(): array;
+    
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Board::class);
+    }
+    
+    public function findAll(): array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT b
+            FROM App\Tasktracker\Entity\Board b
+            ORDER BY b.id ASC'
+        );
+
+        return $query->getResult();
+    }
 }
--- src/Tasktracker/Repository/BoardRepository.php
+++ src/Tasktracker/Repository/BoardRepository.php
@@ -2,28 +2,7 @@
 
 namespace App\Repository;
 
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
-use App\Tasktracker\Entity\Board;
-
-class BoardRepository extends ServiceEntityRepository implements BoardRepository
+interface BoardRepository
 {
-    
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Board::class);
-    }
-    
-    public function findAll(): array
-    {
-        $entityManager = $this->getEntityManager();
-
-        $query = $entityManager->createQuery(
-            'SELECT b
-            FROM App\Tasktracker\Entity\Board b
-            ORDER BY b.id ASC'
-        );
-
-        return $query->getResult();
-    }
+    public function findAll(): array;
 }
