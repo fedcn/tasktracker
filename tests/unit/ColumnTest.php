4428c5b037b8324f3b54ee885892069597fb310a
Yury G <golovanovya@gmail.com>
2020-08-04 22:05:31 +0300

    add column tests

---
+++ tests/unit/ColumnTest.php
@@ -0,0 +1,49 @@
+<?php namespace App\Tests;
+
+class ColumnTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \App\Tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+    
+    /**
+     * test creation new columns
+     * @dataProvider dataProvider
+     */
+    public function testColumnCreate($data)
+    {
+        
+        $user = new \App\Tasktracker\Entity\User('test', 'test@gmail.com');
+        $board = new \App\Tasktracker\Entity\Board('test', $user);
+        foreach ($data as $item) {
+            [$expectedCount, $name] = $item;
+            $column = new \App\Tasktracker\Entity\Column($name);
+            $board->addColumn($column);
+            $this->assertEquals($name, $column->getName());
+            $this->assertEquals($board->getColumnsCount(), $column->getOrder());
+            $this->assertEquals($expectedCount, $board->getColumnsCount());
+        }
+        var_dump($board);exit();
+    }
+    
+    public function dataProvider()
+    {
+        return [
+            [
+                [
+                    [1, 'todo'],
+                    [2, 'work'],
+                ]
+            ],
+        ];
+    }
+}
